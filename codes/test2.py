0856b00e326a732186f052313756a0a574d0f991
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2023-10-05 14:28:57 +0700

    Update gibbs script

--- codes/test2.py
+++ codes/test2.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, '..')
 import numpy as np
 import random
 import qiskit
@@ -6,7 +8,43 @@ import qtm.state
 import qtm.qcompilation
 import qtm.ansatz
 import qtm.constant
-from qtm.evolution import environment, mutate, selection, crossover
+from qtm.evolution import environment, mutate, selection, crossover, utils
 import matplotlib.pyplot as plt
+gibbs_qubits = 2
+beta = 1
 
-qtm.random_circuit.generate_with_pool(3, 5, qtm.constant.operations)
+
+def compilation_fitness(qc: qiskit.QuantumCircuit, num_steps = 5):
+    compiler = qtm.qcompilation.QuantumCompilation(
+        u=qc,
+        vdagger=qtm.state.construct_tfd_state(gibbs_qubits,beta).inverse(),
+        optimizer='adam',
+        loss_func='loss_fubini_study'
+    )
+    compiler.fit(num_steps=num_steps, verbose=0)
+    return np.average(compiler.loss_values)
+def compilation_threshold(fitness_value):
+    if fitness_value < 0.4: 
+        return True
+    return False
+
+params = {'depth': 10,
+          'num_individual': 32,  # Must mod 8 = 0
+          'num_generation': 30,
+          'num_qubits': 4,
+          'threshold': compilation_threshold,
+          'prob_mutate': 0.01}
+
+env = environment.EEnvironment(
+    params,
+    fitness_func = compilation_fitness,
+    selection_func = selection.elitist_selection,
+    crossover_func= crossover.onepoint_crossover,
+    mutate_func=mutate.bitflip_mutate,
+    pool = qtm.constant.operations
+)
+
+env.initialize_population()
+env.evol()
+
+env.save(file_name='../experiments/gibbs_2gen.envobj')
