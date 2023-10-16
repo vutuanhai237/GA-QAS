630478950cd88702bf94b2f5fd6bbf034bba8089
vietnguyen012 <vietnguyen012>
2023-10-16 14:16:08 +0700

    update

--- codes/qtm/state.py
+++ codes/qtm/state.py
@@ -1,5 +1,6 @@
 import qiskit, numpy as np
 import qtm.gate, qtm.encoding
+from qiskit.quantum_info import SparsePauliOp
 
 def w(qc: qiskit.QuantumCircuit, num_qubits: int, shift: int = 0):
     """The below codes is implemented from [this paper](https://arxiv.org/abs/1606.09290)
@@ -86,7 +87,7 @@ def create_haar_state(num_qubits: int):
     psi = 2*np.random.rand(2**num_qubits)-1
     psi = psi / np.linalg.norm(psi)
     qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
-    qc.initialize(psi, qubits = range(0, num_qubits))
+    qc.prepare_state(psi, list(range(0, num_qubits)))
     return qc
 
 def create_haar_state_inverse(num_qubits: int):
@@ -126,7 +127,7 @@ def create_w_state_inverse(qc: qiskit.QuantumCircuit):
     qc = qc.combine(qc1.inverse())
     return qc
 
-def create_AME_state(num_qubits: int):
+def create_ame_state(num_qubits: int):
     if num_qubits == 3:
         amplitude_state = np.array([
                 0.27,
@@ -162,7 +163,7 @@ def create_AME_state(num_qubits: int):
     return qc
 
 
-def create_AME_state_fake(num_qubits: int):
+def create_ame_state_fake(num_qubits: int):
     amplitude_state = np.array([
             0.27,
             0.363,
@@ -176,7 +177,7 @@ def create_AME_state_fake(num_qubits: int):
     qc.prepare_state(amplitude_state, [0,1,2])
     return qc
 
-def create_AME_state_inverse(qc: qiskit.QuantumCircuit):
+def create_ame_state_inverse(qc: qiskit.QuantumCircuit):
     """Create n-qubit W state based on the its number of qubits
 
     Args:
@@ -185,6 +186,72 @@ def create_AME_state_inverse(qc: qiskit.QuantumCircuit):
     Returns:
         - qiskit.QuantumCircuit
     """
-    qc1 = create_AME_state(qc.num_qubits)
+    qc1 = create_ame_state(qc.num_qubits)
     qc = qc.combine(qc1.inverse())
+    return qc
+
+
+def calculate_hamiltonian(num_qubits):
+    # pauli_x = np.array([[0,1],[1,0]])
+    # pauli_z = np.array([[1,0],[0,-1]])
+    # identity = np.array([[1,0],[0,1]])
+    labels = ["XI", "IX","ZZ","ZZ"]
+    coeffs = [1,1,1,1]
+    spo = SparsePauliOp(labels,coeffs)
+    return spo.to_matrix() 
+
+# Calculating the eigenvalues and eigenvectors of the cost Hamiltonian
+
+def find_eigenvec_eigenval(matrix):
+
+    value, vector = np.linalg.eig(matrix)
+    new_vector = []
+    for v in range(0, len(vector)):
+        holder = []
+        for h in range(0, len(vector)):
+            holder.append(vector[h][v])
+        new_vector.append(holder)
+
+    return [value, np.array(new_vector)]
+
+# Preaparing the partition function and each of the probability amplitudes of the diifferent terms in the TFD state
+
+def calculate_terms_partition(eigenvalues,beta):
+
+    list_terms = []
+    partition_sum = 0
+    for i in eigenvalues:
+        list_terms.append(np.exp(-0.5*beta*i))
+        partition_sum = partition_sum + np.exp(-1*beta*i)
+
+    return [list_terms, np.sqrt(float(partition_sum.real))]
+
+#Preparring the TFD state for the cost function
+
+def construct_tfd_state(num_qubits,beta):
+
+    # In this implementation, the eigenvectors of the Hamiltonian and the transposed Hamiltonian are calculated separately
+    y_gate = 1
+    y = np.array([[0, -1], [1, 0]])
+    for i in range(0, num_qubits):
+        y_gate = np.kron(y_gate, y)
+    
+    matrix = calculate_hamiltonian(num_qubits)
+    eigen = find_eigenvec_eigenval(matrix)
+    partition = calculate_terms_partition(eigen[0],beta)
+
+    vec = np.zeros(2**(2*num_qubits))
+    for i in range(0, 2**num_qubits):
+        
+        # time_rev = complex(0,1)*np.matmul(y_gate, np.conj(eigen[1][i]))
+        addition = (float((partition[0][i]/partition[1]).real))*(np.kron(eigen[1][i],eigen[1][i]))
+        vec = np.add(vec, addition)
+
+    qc = qiskit.QuantumCircuit(4,4)
+    amplitude_state = vec/np.sqrt(sum(np.absolute(vec) ** 2))
+    qc.prepare_state(amplitude_state, list(range(0, num_qubits**2)))
+    # rho = qiskit.quantum_info.DensityMatrix.from_instruction(qc)
+    # gibbs_rho = qiskit.quantum_info.partial_trace(rho, [0, 1]).to_statevector()
+    # qc = qiskit.QuantumCircuit(2,2)
+    # qc.prepare_state(gibbs_rho, list(range(0, 2**2)))
     return qc
