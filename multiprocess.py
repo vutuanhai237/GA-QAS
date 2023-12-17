458b7467cb8924454ef6cd12550a42698f596c28
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2023-12-17 22:38:36 +0700

    Update VQE

--- multiprocess.py
+++ multiprocess.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 import math
 
-PRIMES = [
+it_parameters = [
     112272535095293,
     112582705942171,
     112272535095293,
@@ -9,23 +9,23 @@ PRIMES = [
     115797848077099,
     1099726899285419]
 
-def is_prime(n):
-    if n < 2:
+def it_function(parameters):
+    if parameters < 2:
         return False
-    if n == 2:
+    if parameters == 2:
         return True
-    if n % 2 == 0:
+    if parameters % 2 == 0:
         return False
 
-    sqrt_n = int(math.floor(math.sqrt(n)))
+    sqrt_n = int(math.floor(math.sqrt(parameters)))
     for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
+        if parameters % i == 0:
             return False
     return True
 
 def main():
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        for number in (executor.map(is_prime, PRIMES)):
+        for number in (executor.map(it_function, it_parameters)):
             print((number))
 
 if __name__ == '__main__':
