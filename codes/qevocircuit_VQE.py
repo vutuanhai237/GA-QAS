8a4e08ffcaa7552ecbf8564a212340eee99b38f6
user <user@qe2021>
2023-12-31 06:05:59 +0100

    upload data 10 gen

--- codes/qevocircuit_VQE.py
+++ codes/qevocircuit_VQE.py
@@ -15,7 +15,7 @@ h2_631g = lambda distances:  f"H 0 0 {distances[0]}; H 0 0 {distances[1]}"
 h4_sto3g = lambda distances: f"H 0 0 {distances[0]}; H 0 0 {distances[1]}; H 0 0 {distances[2]}; H 0 0 {distances[3]}"
 lih_sto3g = lambda distances: f"Li 0 0 {distances[0]}; H 0 0 {distances[1]}"
 
-file = open('Hydrogen molecules-631g-200.txt', 'r').readlines()
+file = open('Li-H molecules-631g-200.txt', 'r').readlines()
 
 def exact_VQE_H2_631g(distances: []):
     return 
@@ -23,8 +23,8 @@ def exact_VQE_H2_631g(distances: []):
 def general_VQE_H2_631g(distances: []):
     def VQE_H2_631g(qc: qiskit.QuantumCircuit):
         return VQE_fitness(qc, 
-                           # Replace atom here, below function returns text such as "H 0 0 0; H 0 0 0.25"
-                           atom = h2_631g(distances), 
+                           # Replace atom here, below function returns text such as "Li 0 0 0; H 0 0 0.25"
+                           atom =lih_sto3g(distances), 
                            # Replace basis here
                            basis = "631g") #631g
     return VQE_H2_631g
@@ -45,7 +45,7 @@ def VQE_fitness(qc: qiskit.QuantumCircuit, atom: str, basis: str) -> float:
     exact_value = eval(file[round((eval(atom.split(' ')[-1])-0.25)/((2.5-0.25)/199))].split(" ")[-2])
     return utilities.similarity(computation_value, exact_value)
 
-def VQE_H2_631g_fitness(qc: qiskit.QuantumCircuit) -> float:
+def VQE_LiH_631g_fitness(qc: qiskit.QuantumCircuit) -> float:
     """Fitness function for H2_631g case
 
     Args:
@@ -54,7 +54,7 @@ def VQE_H2_631g_fitness(qc: qiskit.QuantumCircuit) -> float:
     Returns:
         float: fitness value
     """
-    num_points = 6
+    num_points = 10
     # Create pairs of distanc
     list_distances_H2_631g = list(zip([0]*num_points, np.linspace(0.5, 2.5, num_points))) 
     fitnesss = []
@@ -71,13 +71,13 @@ env_metadata = EEnvironmentMetadata(
     num_qubits=8,
     depth=3,
     num_circuit=4,
-    num_generation=5,
+    num_generation=10,
     prob_mutate=3/(3 * 4)  # Mutation rate / (depth * num_circuit)
 )
 env = EEnvironment(
     metadata=env_metadata,
     # Fitness function alway has the function type: qiskit.QuantumCircuit -> float
-    fitness_func=VQE_H2_631g_fitness,
+    fitness_func=VQE_LiH_631g_fitness,
     selection_func=selection.elitist_selection,
     crossover_func=crossover.onepoint_crossover,
     mutate_func=mutate.layerflip_mutate,
@@ -87,13 +87,13 @@ env = EEnvironment(
 # Automatically save the results in the same level folder
 env.evol(1)
 
-computation_value = vqe.general_VQE(env.best_circuit, h2_631g([0,0.25]), basis='sto3g')
+computation_value = vqe.general_VQE(env.best_circuit, lih_sto3g([0,0.25]), basis='631g')
 print(computation_value)
 
 # Load the result from folder
 env2 = EEnvironment.load(
-    './4qubits_VQE_H2_631g_fitness_2023-12-30', 
-    VQE_H2_631g_fitness
+    './8qubits_VQE_H2_631g_fitness_2023-12-30', 
+   VQE_LiH_631g_fitness
 )
 
 env2.plot()
