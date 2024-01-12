cea66c9d1792ddbcf1415db9afa1d465c7103faf
user <user@qe2021>
2024-01-12 17:51:11 +0100

    H2 latest update

---
+++ codes/Test_parallel_computing_HPC.py
@@ -0,0 +1,35 @@
+import time
+import numpy as np
+import concurrent.futures
+import os
+
+def main():
+
+    file = open("Test_HPC.txt", "w")
+    file.write("Number of CPU: {} \n" ,os.cpu_count())
+
+    def square(x):
+        return x**2
+    
+    number = 10**3
+    start_time = time.time()
+
+    executor = concurrent.futures.ProcessPoolExecutor()
+    results = executor.map(square,  range(1, number))
+
+    file.write("Parrallel: {} \n", time.time() - start_time)
+
+
+    results = [square(x) for x in range(1, number)]  #46s, 56s
+
+    start_time = time.time()
+
+    file.write("Serial: {} \n", time.time() - start_time)
+
+
+    file.write(str(list(results)))
+
+    file.close()
+
+if __name__ == '__main__':
+    main()
